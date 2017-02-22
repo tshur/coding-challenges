d41a47a5944c058c912b4f507dfe3c382a3045b8
Tim Shur <tim.shur@gmail.com>
2017-02-21 17:51:51 -0800

    Add files
    Added files for path-triples program.

---
+++ pyth-triples/cmake-build-debug/CMakeFiles/pyth_triples.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pyth_triples.dir/main.cpp.o"
+  "pyth_triples.pdb"
+  "pyth_triples"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pyth_triples.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pyth-triples/cmake-build-debug/CMakeFiles/pyth_triples.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for pyth_triples.
+# This may be replaced when dependencies are built.
